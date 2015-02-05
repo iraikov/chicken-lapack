8a654d3393559c802576fd64b99a438fce4585c2
Ivan Raikov <ivan.g.raikov@gmail.com>
2015-02-05 07:29:08 -0800

    updated copyright notice

--- atlas-lapack.scm
+++ atlas-lapack.scm
@@ -2,7 +2,7 @@
 ;; Chicken Scheme bindings for the LAPACK routines in the ATLAS
 ;; library.
 ;;
-;; Copyright 2007-2012 Ivan Raikov and the Okinawa Institute of Science and Technology
+;; Copyright 2007-2015 Ivan Raikov
 ;;
 ;; This program is free software: you can redistribute it and/or
 ;; modify it under the terms of the GNU General Public License as
