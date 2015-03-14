242ee63483dc3ea9c5b94c05e69863e843270697
Jeremy Steward <jeremy@thatgeoguy.ca>
2015-03-14 13:21:37 -0600

    Added test for running dgeev_ properly Ideally I'll add asserts and the like in here eventually For now I can't even get dgeev_ to execute properly with all its arguments.

---
+++ tests/dgeev_.scm
@@ -0,0 +1,16 @@
+(use srfi-4 lapack-extras)
+
+(define N (s32vector 3))
+(define A (f64vector 1 2 3 4 5 6 7 8 9))
+(define LDA (s32vector 3))
+(define WR (make-f64vector 3 0))
+(define WI (make-f64vector 3 0))
+(define VL (make-f64vector 3 0))
+(define VR (make-f64vector 3 0))
+(define LDVL (s32vector 3))
+(define LDVR (s32vector 3))
+(define WORK (make-f64vector 12 0))
+(define LWORK (s32vector 12))
+(define INFO (f64vector 0))
+
+(dgeev_ "N" "N" N A LDA WR WI VL LDVL VR LDVR WORK LWORK INFO)
