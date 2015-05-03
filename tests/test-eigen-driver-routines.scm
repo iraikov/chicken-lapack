1e8307981c6187e4eea37c1ee5317999c818c02b
Jeremy Steward <jeremy@thatgeoguy.ca>
2015-05-03 12:43:20 -0600

    Adds tests for (c|z)geev_ procedures Removes old tests in dgeev_.scm This should be sufficient to close Issue #2 on Github.

--- tests/test-eigen-driver-routines.scm
+++ tests/test-eigen-driver-routines.scm
@@ -23,7 +23,13 @@
      lapack-extras
      test)
 
-(test-group "Test (s|d|c|z)geev_ procedures"
+;; NOTE: for the identity tests that the geev_ procedures read in values in
+;; column-major order (they are really just CHICKEN->C->Fortran bindings after
+;; all, and Fortran always uses Column Major order). However, since the matrix
+;; is symmetric, it ends up appearing the same as if they were in row major
+;; order.
+
+(test-group "Test sgeev_ procedure"
   (test "sgeev_ eigenvalues / loadings of identity are identity"
         (values '#f32(1 1 1)
                 '#f32(0 0 0)
@@ -55,8 +61,9 @@
                               WORK
                               LWORK
                               INFO)])
-            (values wr wi vr))))
+            (values wr wi vr)))))
 
+(test-group "Test dgeev_ procedure"
   (test "dgeev_ eigenvalues / loadings of identity are identity"
         (values '#f64(1 1 1)
                 '#f64(0 0 0)
@@ -88,5 +95,70 @@
                                WORK
                                LWORK
                                INFO)])
-            (values wr wi vr))))
-  )
+            (values wr wi vr)))))
+
+(test-group "Test cgeev_ procedure"
+  (test "cgeev_ eigenvalues / loadings of identity are identity"
+        (values '#f32(1 0 1 0 1 0)
+                '#f32(1 0 0 0 0 0
+                      0 0 1 0 0 0
+                      0 0 0 0 1 0))
+        (let* ([jobvl "N"]
+               [jobvr "V"]
+               [N 3]
+               [A (f32vector 1 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 1 0)]
+               [W (make-f32vector (* 2 N) 0)]
+               [LDVL N]
+               [VL (make-f32vector (* 2 LDVL N) 0)]
+               [LDVR N]
+               [VR (make-f32vector (* 2 LDVR N) 0)]
+               [LWORK (* 4 N)]
+               [WORK (make-f32vector LWORK 0)]
+               [RWORK (make-f32vector (* 2 N) 0)]
+               [INFO (s32vector 0)])
+          (let-values ([(a w vl vr work)
+                        (cgeev jobvl
+                               jobvr
+                               N
+                               A
+                               W
+                               VL
+                               VR
+                               WORK
+                               LWORK
+                               RWORK
+                               INFO)])
+            (values w vr)))))
+
+(test-group "Test zgeev_ procedure"
+  (test "zgeev_ eigenvalues / loadings of identity are identity"
+        (values '#f64(1 0 1 0 1 0)
+                '#f64(1 0 0 0 0 0
+                      0 0 1 0 0 0
+                      0 0 0 0 1 0))
+        (let* ([jobvl "N"]
+               [jobvr "V"]
+               [N 3]
+               [A (f64vector 1 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 1 0)]
+               [W (make-f64vector (* 2 N) 0)]
+               [LDVL N]
+               [VL (make-f64vector (* 2 LDVL N) 0)]
+               [LDVR N]
+               [VR (make-f64vector (* 2 LDVR N) 0)]
+               [LWORK (* 4 N)]
+               [WORK (make-f64vector LWORK 0)]
+               [RWORK (make-f64vector (* 2 N) 0)]
+               [INFO (s32vector 0)])
+          (let-values ([(a w vl vr work)
+                        (zgeev jobvl
+                               jobvr
+                               N
+                               A
+                               W
+                               VL
+                               VR
+                               WORK
+                               LWORK
+                               RWORK
+                               INFO)])
+            (values w vr)))))
